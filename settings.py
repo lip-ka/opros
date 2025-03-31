944cb5754e3ab461862bf0cb22e6d5683653194c
lip-ka <7gbvfhxg@gmail.com>
2025-04-01 01:07:21 +0300

    test

--- settings.py
+++ settings.py
@@ -29,7 +29,7 @@ DATABASES = {
 # if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
 # in SESSION_CONFIGS, except those that explicitly override it.
 # the session config can be accessed from methods in your apps as self.session.config,
-# e.g. self.session.config['participation_fee']
+# e.g self.session.config['participation_fee']
 
 SESSION_CONFIG_DEFAULTS = dict(
     real_world_currency_per_point = 1.00, participation_fee = 0.00, doc = ""
